c2057638729f4d1c7890d73b3d3e7b21552f3918
xxxZer0xxx <max.reed@hotmail.com>
2020-03-07 19:47:54 +0100

    removed FragmentManager

--- app/src/main/java/com/maxreed/listmaker/MainActivity.kt
+++ app/src/main/java/com/maxreed/listmaker/MainActivity.kt
@@ -32,10 +32,6 @@ class MainActivity : AppCompatActivity(), TodoListFragment.OnFragmentInteraction
             showCreateTodoListDialog()
         }
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_container, todoListFragment)
-            .commit()
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/maxreed/listmaker/TaskDetailFragment.kt
@@ -0,0 +1,29 @@
+package com.maxreed.listmaker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+
+class TaskDetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    companion object {
+        fun newInstance(list: TaskList) = TaskDetailFragment()
+    }
+
+}
