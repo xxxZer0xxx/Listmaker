c17156c5bbbed9adab8f954563a803189843f9cd
xxxZer0xxx <max.reed@hotmail.com>
2020-03-07 20:09:24 +0100

    updated todolistfragment for single activity use

--- app/src/main/java/com/maxreed/listmaker/MainActivity.kt
+++ app/src/main/java/com/maxreed/listmaker/MainActivity.kt
@@ -1,22 +1,11 @@
 package com.maxreed.listmaker
 
 
-import android.content.Intent
 import android.os.Bundle
-import android.text.InputType
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), TodoListFragment.OnFragmentInteractionListener {
-
-
-    private var todoListFragment = TodoListFragment.newInstance()
+class MainActivity : AppCompatActivity() {
 
     companion object{
         const val INTENT_LIST_KEY = "list"
@@ -28,73 +17,6 @@ class MainActivity : AppCompatActivity(), TodoListFragment.OnFragmentInteraction
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { _->
-            showCreateTodoListDialog()
-        }
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == LIST_DETAIL_REQUEST_CODE) {
-            data?.let {
-                val list: TaskList = data.getParcelableExtra<TaskList>(INTENT_LIST_KEY)
-                todoListFragment.saveList(list)
-                todoListFragment.updateLists()
-            }
-        }
-    }
-
-
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    private fun showCreateTodoListDialog() {
-
-        val dialogTitle = getString(R.string.name_of_list)
-        val positiveButtonTitle = getString(R.string.create_list)
-        val myDialog = AlertDialog.Builder(this)
-        val todoTitleEditText = EditText(this)
-        todoTitleEditText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_CAP_WORDS
-
-        myDialog.setTitle(dialogTitle)
-        myDialog.setView(todoTitleEditText)
-
-
-        myDialog.setPositiveButton(positiveButtonTitle) {
-            dialog, _->
-            val list = TaskList(todoTitleEditText.text.toString())
-            todoListFragment.addList(list)
-            dialog.dismiss()
-            showTaskListsItems(list)
-        }
-
-        myDialog.create().show()
-    }
-
-    private fun showTaskListsItems(list: TaskList) {
-        val taskListItem = Intent(this, DetailActivity::class.java)
-        taskListItem.putExtra(INTENT_LIST_KEY, list)
-        startActivityForResult(taskListItem, LIST_DETAIL_REQUEST_CODE) // requestCode uniqueIdentifier
-    }
-
-    override fun onTodoListClicked(list: TaskList) {
-       showTaskListsItems(list)
     }
 
 }
--- app/src/main/java/com/maxreed/listmaker/TodoListFragment.kt
+++ app/src/main/java/com/maxreed/listmaker/TodoListFragment.kt
@@ -1,19 +1,21 @@
 package com.maxreed.listmaker
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_todo_list.*
 
 class TodoListFragment : Fragment(), TodoListAdapter.TodoListClickListener {
-
-
-    private var listener: OnFragmentInteractionListener? = null
 
     private lateinit var todoListRecyclerView: RecyclerView
     private lateinit var listDataManager: ListDataManager
@@ -32,25 +34,23 @@ class TodoListFragment : Fragment(), TodoListAdapter.TodoListClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        activity?.let {
+            listDataManager = ListDataManager(it)
+        }
+
+
         val lists = listDataManager.readLists()
         todoListRecyclerView = view.findViewById(R.id.lists_recyclerview)
         todoListRecyclerView.layoutManager = LinearLayoutManager(activity) // could use GridManager for Gridlayout
         todoListRecyclerView.adapter = TodoListAdapter(lists, this)
-    }
 
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-            listDataManager = ListDataManager(context)
+        fab.setOnClickListener { _->
+            showCreateTodoListDialog()
         }
+
     }
 
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
 
     interface OnFragmentInteractionListener {
         fun onTodoListClicked(list: TaskList)
@@ -63,7 +63,7 @@ class TodoListFragment : Fragment(), TodoListAdapter.TodoListClickListener {
     }
 
     override fun listItemClicked(list: TaskList) {
-        listener?.onTodoListClicked(list)
+
     }
 
     fun addList(list: TaskList) {
@@ -77,8 +77,40 @@ class TodoListFragment : Fragment(), TodoListAdapter.TodoListClickListener {
         updateLists()
     }
 
+    private fun showCreateTodoListDialog() {
+
+        activity?.let {
+            val dialogTitle = getString(R.string.name_of_list)
+            val positiveButtonTitle = getString(R.string.create_list)
+            val myDialog = AlertDialog.Builder(it)
+            val todoTitleEditText = EditText(it)
+            todoTitleEditText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_CAP_WORDS
+
+            myDialog.setTitle(dialogTitle)
+            myDialog.setView(todoTitleEditText)
+
+
+            myDialog.setPositiveButton(positiveButtonTitle) {
+                    dialog, _->
+                val list = TaskList(todoTitleEditText.text.toString())
+                addList(list)
+                dialog.dismiss()
+                showTaskListsItems(list)
+            }
+
+            myDialog.create().show()
+        }
+
+
+    }
+
+    private fun showTaskListsItems(list: TaskList) {
+
+    }
+
     fun updateLists() {
         val lists = listDataManager.readLists()
         todoListRecyclerView.adapter = TodoListAdapter(lists, this)
     }
+
 }
