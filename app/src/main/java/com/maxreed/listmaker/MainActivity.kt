99db3242cc126cf17e1c0edfe2fda3b5e7fdcaca
xxxZer0xxx <max.reed@hotmail.com>
2020-03-07 19:22:22 +0100

    created fragment

--- app/src/main/java/com/maxreed/listmaker/MainActivity.kt
+++ app/src/main/java/com/maxreed/listmaker/MainActivity.kt
@@ -15,6 +15,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), TodoListAdapter.TodoListClickListener {
 
+    private lateinit var todoListRecyclerView: RecyclerView
+    private lateinit var listDataManager: ListDataManager
+
     companion object{
         const val INTENT_LIST_KEY = "list"
         const val LIST_DETAIL_REQUEST_CODE = 123
